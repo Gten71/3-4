e136e2ec0f954809d139b11224eaeda16fc4937d
Maxim <Maxim@DESKTOP-ICMSR5Q>
2021-11-11 13:47:33 +0200

    Add project files.

---
+++ 3,4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|     x       |      y       |");
+            for (double x = 0.1; x <= 2.5; x += 0.2)
+            {
+                double y = Math.Sqrt(x) - 2 * Math.Cos(0.5 * Math.PI * x);
+                Console.WriteLine("|{0,13:F1}|{1,14:F2}|", x, y);
+            }
+        }
+    }
+}
